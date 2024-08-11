49820fbe564bb5cf9ad03a3af87021b8fcb5b3b7
Barath Subramaniam <barsubra@adobe.com>
2024-08-11 13:13:25 -0700

    delete

--- gen_ui_backend/types.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Union
-
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
-from langchain_core.pydantic_v1 import BaseModel
-
-
-class ChatInputType(BaseModel):
-    input: List[Union[HumanMessage, AIMessage, SystemMessage]]
